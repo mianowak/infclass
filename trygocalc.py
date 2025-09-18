259a6ff58838717b57605a05e6ecd7d49581c988
Mia Nowak <nowak.mia@icloud.com>
2025-09-18 17:44:49 +0200

    dodalam wiecej figur

--- trygocalc.py
+++ trygocalc.py
@@ -1,14 +1,19 @@
-
+import math
+# wartosci liczby pi
+pi = math.pi
 
 print('''
     Chcesz pracować z figurami czy bryłami
     a - Figury
     b - Bryły
+    c - obwod figury
+    d- obwod bryły
 
     ''')
 
 
-menu0 = input("a / b ? ")
+menu0 = input("a / b / c / d? ")
+
 if "a" == menu0:
     print('''
     wybierz co chcesz policzyć
@@ -65,6 +70,11 @@ elif "b" == menu0:
     a - pp szescianu
     b - pp prostoadloscianu
     c - pp graniastoslup prosty 
+    d - pp ostroslup 
+    e - pp walec
+    f - pp stozek
+    g - pp kula
+    
     ''')
     inp = input("?")
     if inp == "a":
@@ -83,5 +93,124 @@ elif "b" == menu0:
         b = float(input("podaj dlugosc krawedzi (b):"))
         pcg = 2*a + b
         print(f"pole calkowitwe graniastoslupa o krawedzi {a} i krawedzi {b} = {pcg}")
+
+    elif inp == "d":
+        a = float(input("podaj dlugosc pola podstawy (a)"))
+        b = float(input("podaj dlugodc pola bocznego (b)"))
+        pco = a + b
+        print(f"pole calkowite ostroslupa o polu podstawy {a} i polu bocznego {b} = {pco}")
+
+    elif inp == "e":
+        r = float(inp("podaj dlugosc promienia podstawy (r): "))
+        H = float(input("podaj dlugosc wysokosci walca (H):"))
+        pcw = 2 * pi * r * (r + H)
+        print(f"podaj pole calkowite walca o promieniu podstawy {r} i wysokosci {H} = {pcw}")
+
+    elif inp == "f":
+        r = float(input("podaj dlugosc promienia podstawy (r):"))
+        H = float(input("podaj dlugosc wysokosci stozka (H):"))
+        pcs = pi * r**2 + pi * r * H
+        print(f"podaj pole calkowite stozka o promieniu podstawy {r} i wysokosci {H} = {pcs}")
+
+    elif inp == "g":
+        r = float(input("podaj dlugosc promienia kuli (r):"))
+        pk = 4/3 * pi * r**3
+        print(f"podaj pole calkowite kuli o promieniu {r} = {pk}")
+
+    elif "c" == menu0:
+        print("Wybierz figure:")
+        print("a - kwadrat")
+        print("b - prostokat")
+        print("c - trojkat")
+        print("d - trapez")
+        print("e - rownoleglobok")
+        print("f - romb")
+        print("g - kola")
+    
+    inp = input("?")
+
+    if inp == "a":
+        a = float(input("podaj dlugosc boku kwadratu (a):"))
+        print(f"obwod kwadratu o boku {a} = {4*a}")
+
+    elif inp == "b":
+        a = float(input("podaj dlugosc boku (a):"))
+        b = float(input("podaj dlugosc boku (b):"))
+        print(f"obwod prostokata o bokach {a} i {b} = {2*(a+b)}")
+        
+    elif inp == "c":
+        a = float(input("podaj dligosc boku (a):"))
+        b = float(input("podaj dlugosc boku (b):"))
+        c = float(input("podaj dlugosc boku (c):"))
+        print(f" obwod trojkata o bokach {a} i {b} i {c} = {a+b+c}")
+        
+
+    elif inp == "d":
+        a = float(input("podaj dlugosc boku (a):"))
+        b = float(input("podaj dlugosc boku (b):"))
+        c = float(input("podaj dlugosc boku (c):"))
+        d = float(input("podaj dlugosc boku (d):"))
+        print(f"obwod trapezu o bokach {a} i {b} i {c} i {d} = {a+b+c+d}")
+
+    elif inp == "e":
+        a = float(input("podaj dlugosc boku (a):"))
+        b = float(input("podaj dlugosc boku (b):"))
+        print(f"obwod rownolegloboku o bokach {a} i {b} = {2*a + 2*b }")
+
+
+    elif inp == "f":
+        a = float(input("podaj dlugosc boku (a):"))
+        b = float(input("podaj dlugosc boku (b):"))
+        print(f"obwod rombu o bokach {a} i {b} = {4*a}")
+
+    elif inp == "g":
+        r = float(input("podaj dlugosc promienia (r):"))
+        print(f"obwod kola o promieniu {r} = {2*pi*r}")
+
+    elif "d" == menu0:
+        print("objetosci bryl")
+        print("a - o szescian")
+        print("b - o prostopadlocian")
+        print("c - o graniastoslup")
+        print("d - o ostroslup")
+        print("e - o walec")
+        print("f - o stozek")
+        print("g - kula")
+
+    if inp == "a":
+        a = float(input("podaj dlugosc krawedzi (a):"))
+        print(f"objetosc szescianu o krawedzi {a} = {a**3}")
+
+    elif inp == "b":
+        a = float(input("podaj dlugosc krawedzi (a):"))
+        b = float(input("podaj dlugosc krawedzi (b):"))
+        c = float(input("podaj dlugosc krawedzi (c):"))
+        print(f"objetosc prostopadlocianu o krawedzi {a} i krawedzi {b} i krawedzi {c} = {a*b*c}")
+
+    elif inp == "c":
+        a = float(input("podaj dlugosc krawedzi (a): "))
+        b = float(input("podaj dlugosc krawedzi (b):"))
+        H = float(input("podaj dlugosc wysokosci (H):"))
+        print(f"objetosc graniastoslupa o krawedzi {a} i krawedzi {b} i wysokosci {H} = {a*b*H}")
+
+    elif inp == "d":
+        a = float(input("podaj dlugosc krawedzi (a): "))
+        H = float(input("podaj dlugosc wysokosci (H):"))
+        print(f"objetosc ostroslupa o krawedzi {a} i wysokosci {H} = {1/3*a**2*H}")
+
+    elif inp == "e":
+        r = float(input("podaj dlugosc promienia podstawy (r): "))
+        H = float(input("podaj dlugosc wysokosci walca (H):"))
+        print(f"objetosc walca o promieniu podstawy {r} i wysokosci {H} = {pi*r**2*H}")
+
+    elif inp == "f":
+        r = float(input("podaj dlugosc promienia podstawy (r):"))
+        H = float(input("podaj dlugosc wysokosci stozka (H):"))
+        print(f"objetosc stozka o promieniu podstawy {r} i wysokosci {H} = {1/3*pi*r**2*H}")
+
+    elif inp == "g":
+        r = float(input("podaj dlugosc promienia kuli (r):"))
+        print(f"objetosc kuli o promieniu {r} = {4/3*pi*r**3}")
+
 else:
     print("Nie ma takiej komendy")
